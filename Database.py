9a2d2cb09de72d2e6803f16b3870cd39a15d057c
alichahardowly <alichahardowly9000893@gmail.com>
2024-08-05 17:53:21 +0330

    insertdb
    append a python file for insert data in database

--- Database.py
+++ Database.py
@@ -81,17 +81,6 @@ class database_for_details:
         except:
             connection.rollback()
             print("Selecting from database failed...")
-        
-x=database_for_details("pcRamDetails","postgres","1234","127.0.0.1","5432")
-x.create_table()
-def information():
-    p=pc_details()
-    timestamp=str(int(time.time()))
-    total=p[0]
-    free=p[1]
-    used=p[2]
-    x.insert_to_database(timestamp,total,free,used)
-while True:
-    information()
-    print("Inserting to database succeeded...")
-    time.sleep(1)
+x=database_for_details("pcRamDetails","postgres","1234","127.0.0.1","5432")     
+xx=x.list_of_information()  
+print(xx)
---
+++ insertdb.py
@@ -0,0 +1,16 @@
+from backend import pc_details
+import time
+from database import database_for_details
+x=database_for_details("pcRamDetails","postgres","1234","127.0.0.1","5432")
+x.create_table()
+def information():
+    p=pc_details()
+    timestamp=str(int(time.time()))
+    total=p[0]
+    free=p[1]
+    used=p[2]
+    x.insert_to_database(timestamp,total,free,used)
+while True:
+    information()
+    print("Inserting to database succeeded...")
+    time.sleep(1)
